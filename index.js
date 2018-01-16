298bd6eb96c140947cff5bef75c427fece5407f5
Jordan Jaffe <jordan.m.jaffe@gmail.com>
2018-01-16 22:22:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]:value})
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+  object[key] = value
+  return object
 }
+object[key] = value
+
+return Object.assign({}, object, )
