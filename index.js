b2ea643a92c05ff7f5fff60ca036e7a31cf4da4e
Jordan Jaffe <jordan.m.jaffe@gmail.com>
2018-01-16 22:32:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-  const new obj = 
+  const newObj = 
   
   destructivelyDeleteFromObjectByKey
 }
